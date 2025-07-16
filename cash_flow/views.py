9897387a60e588267ebc44aacbeab29e50c5bc6c
Tamir Mandreev <tamirmandreev@mail.ru>
2025-07-17 03:16:41 +0800

    Создал представление для отображения списка транзакций

--- cash_flow/views.py
+++ cash_flow/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+
+from cash_flow.models import Transaction
+
+
+class TransactionListView(ListView):
+    '''Представление для отображения списка транзакций'''
+    model = Transaction
+

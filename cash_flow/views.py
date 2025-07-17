79b1a8c6b818644b2e92ad31cb0bfbd4dd6e0765
Tamir Mandreev <tamirmandreev@mail.ru>
2025-07-17 13:18:28 +0800

    Сделал базовую реализацию страницы для отображения списка транзакций

--- cash_flow/views.py
+++ cash_flow/views.py
@@ -8,4 +8,6 @@ from cash_flow.models import Transaction
 class TransactionListView(ListView):
     '''Представление для отображения списка транзакций'''
     model = Transaction
+    template_name = 'cash_flow/transaction_list.html'
+    context_object_name = 'transactions'
 

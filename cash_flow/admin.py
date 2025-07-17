8e4b0989e2342d6e9593fc331e10ab43787eee25
Tamir Mandreev <tamirmandreev@mail.ru>
2025-07-17 12:50:28 +0800

    Зарегистрировал все классы в панели администратора

--- cash_flow/admin.py
+++ cash_flow/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction, Status, OperationType, Category, SubCategory
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('created_at', 'status', 'operation_type', 'category', 'subcategory', 'amount', 'comment')
+    list_filter = ('created_at', 'status', 'operation_type', 'category', 'subcategory')
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(OperationType)
+class OperationTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'operation_type')
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')

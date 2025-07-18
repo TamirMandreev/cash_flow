0067ec56e9d5f4d0b15d010ab5e04c644c5ca2e9
Tamir Mandreev <tamirmandreev@mail.ru>
2025-07-18 08:45:16 +0800

    Реализовал страницу для обноввления транзакции

--- cash_flow/urls.py
+++ cash_flow/urls.py
@@ -4,4 +4,5 @@ from cash_flow import views
 urlpatterns = [
     path('transactions/', views.TransactionListView.as_view(), name='transactions' ),
     path('transactions/create/', views.TransactionCreateView.as_view(), name='transaction_create' ),
+    path('transactions/update/<int:pk>/', views.TransactionUpdateView.as_view(), name='transaction_update' ),
 ]
--- cash_flow/views.py
+++ cash_flow/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 
 from cash_flow.filters import TransactionFilter
 from cash_flow.models import Transaction
@@ -44,5 +44,12 @@ class TransactionCreateView(CreateView):
     # Поля, которые будут отображться в форме
     fields = '__all__'
     # Куда пользователь будет перенаправлен после успешной отправки формы
+    success_url = reverse_lazy('transactions')
+
+
+class TransactionUpdateView(UpdateView):
+    '''Представление для обновления транзакции'''
+    model = Transaction
+    fields = '__all__'
     success_url = reverse_lazy('transactions')
 
